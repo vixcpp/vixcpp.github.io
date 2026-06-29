9cdbbce14bfdae9ed8651b901f11ef0b8413ffe1
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-06-29 15:40:05 +0300

    Update website architecture

--- vix-site/src/lib/useBodyClass.js
+++
@@ -1,6 +0,0 @@
-import { onMounted, onBeforeUnmount } from "vue";
-
-export function useBodyClass(className) {
-  onMounted(() => document.body.classList.add(className));
-  onBeforeUnmount(() => document.body.classList.remove(className));
-}
